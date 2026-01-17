a64c9aacf14d4e19cc0a9db312708fb1b07dbbdf
Ahmidou EL mehdi <ahmidouem@gmail.com>
2026-01-17 14:11:31 +0100

    chore: setup development branch with local config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://feedback-restaurant.vercel.app',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
